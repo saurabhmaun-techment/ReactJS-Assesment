cf28b87462ad077b61430ea5108614adf751c431
Saurabh Maun <saurabh.maun@techment.com>
2020-05-09 15:30:27 +0530

    [MOD] Form Generated & Submit Button Disabled if Country is empty

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,16 +4,36 @@ export default class Homepage extends React.Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            countryText:""
+        }
+    }
 
+    handleCountryInputChange = (e) => {
+        if(e.target && e.target.value){
+            this.setState({
+                countryText: e.target.value    
+            })
+        } else {
+            this.setState({
+                countryText: ""    
+            })
+        }
     }
 
     render(){
         return(
             <section>
                 <div className="container">
-                    <div className="row">
+                    <div className="row mt-5">
                         <div className="col">
-
+                            <form>
+                                <div class="form-group">
+                                    <label for="country">Email address</label>
+                                    <input type="text" className="form-control" id="country" placeholder="Enter country" onChange={this.handleCountryInputChange} />
+                                </div>
+                                <button type="submit" className="btn btn-primary" disabled={!this.state.countryText}>Submit</button>
+                            </form>
                         </div>
                     </div>
                 </div>
