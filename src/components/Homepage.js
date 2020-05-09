7abe06f78fc9f954da66f0a82630a2fd9b6787d0
Saurabh Maun <saurabh.maun@techment.com>
2020-05-09 15:23:32 +0530

    [MOD] Added Router Bootstrap

---
+++ src/components/Homepage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class Homepage extends React.Component{
+
+    constructor(props){
+        super(props);
+
+    }
+
+    render(){
+        return(
+            <section>
+                <div className="container">
+                    <div className="row">
+                        <div className="col">
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+
+}
